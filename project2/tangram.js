cc21e08d143ff095a67a781bf719396bb5b185a9
amdonham <amdonham@crimson.ua.edu>
2016-02-14 14:15:28 -0600

    Got rotation and click and drag working

--- project2/tangram.js
+++ project2/tangram.js
@@ -22,8 +22,17 @@ var tranxLock;
 var tranyLock;
 var tranx = 0.0;
 var trany = 0.0;
-var redraw = false;
+var redrawT1 = false;
+var redrawT2 = false;
 var program;
+var prevx = 0;
+var prevy = 0;
+
+var rotate = false;
+var rotateT1 = false;
+var thetaT1 = 0.0;
+var rotateT2 = false;
+var thetaT2 = 0.0;
 
 window.onload = function init()
 {
@@ -34,21 +43,85 @@ window.onload = function init()
     
 	
 	canvas.addEventListener("mousedown", function(event){
-	  redraw = true;
+					
+	    //console.log(inside([2*event.clientX/canvas.width - 1, 1 - 2*event.clientY/canvas.height],triangleTwo));
+	    //console.log([2*event.clientX/canvas.width - 1, 1 - 2*event.clientY/canvas.height]);
+	    if (inside([2*event.clientX/canvas.width - 1,1 - 2*event.clientY/canvas.height],triangleOne)){
+	    	redrawT1 = true;
+			if(rotate){rotateT1 = true;}
+	    }
+	    if (inside([2*event.clientX/canvas.width - 1,1 - 2*event.clientY/canvas.height],triangleTwo)){
+		    redrawT2 = true;
+			if(rotate){rotateT2 = true;}
+	    }
+		if(rotateT1){
+			var s = Math.sin( .05 );
+			var c = Math.cos( .05 );
+			for(var i = 1; i < triangleOne.length;i++){
+				var originx = triangleOne[0][0]; var originy = triangleOne[0][1];
+				triangleOne[i][0]-=originx;triangleOne[i][1]-=originy;
+				var x = -s * triangleOne[i][1] + c * triangleOne[i][0];
+				var y = s * triangleOne[i][0] + c * triangleOne[i][1];
+				triangleOne[i][0] = x + originx; triangleOne[i][1] = y + originy;
+			}
+			rotateT1 = false;
+		}
+		if(rotateT2){
+			var s = Math.sin( .05 );
+			var c = Math.cos( .05 );
+			for(var i = 1; i < triangleTwo.length;i++){
+				var originx = triangleTwo[0][0]; var originy = triangleTwo[0][1];
+				triangleTwo[i][0]-=originx;triangleTwo[i][1]-=originy;
+				var x = -s * triangleTwo[i][1] + c * triangleTwo[i][0];
+				var y = s * triangleTwo[i][0] + c * triangleTwo[i][1];
+				triangleTwo[i][0] = x + originx; triangleTwo[i][1] = y + originy;
+			}
+			rotateT2 = false;
+		}
+	  //console.log(2*event.clientX/canvas.width - 1);
     });
 
     canvas.addEventListener("mouseup", function(event){
-      redraw = false;
+      redrawT1 = false;
+	  redrawT2 = false;
     });
 	
 	canvas.addEventListener("mousemove", function(event){
-        if(redraw) {
-			tranx = 2*event.clientX/canvas.width-1;
-			trany = 2*event.clientY/canvas.height-1;
-
-      }
+		tranx = (2*event.clientX/canvas.width-1)- prevx;
+		trany = (1 - 2*event.clientY/canvas.height) - prevy;
+		prevx = 2*event.clientX/canvas.width-1;
+		prevy = 1 - 2*event.clientY/canvas.height;
+		if(redrawT1) {
+			//console.log(inside([0,.5],triangleTwo));		
+			for(var i = 0; i < triangleOne.length;i++){triangleOne[i][0] += tranx; triangleOne[i][1] += trany;}
+		}
+		if(redrawT2) {
+			//console.log(inside([0,.5],triangleTwo));		
+			for(var i = 0; i < triangleTwo.length;i++){triangleTwo[i][0] += tranx; triangleTwo[i][1] += trany;}
+		}
+		
 
     } );
+	window.addEventListener("keydown", function(event){
+		//console.log(event.keyCode);
+		switch(event.keyCode){
+			case 17:
+				
+				rotate = true;
+				break;
+		}
+		
+    });
+	window.addEventListener("keyup", function(event){
+		//console.log(event.keyCode);
+		switch(event.keyCode){
+			case 17:
+				rotate = false;
+				break;
+		}
+		
+    });
+	
 	//
     //  Configure WebGL
     //
@@ -74,7 +147,8 @@ window.onload = function init()
 	tranyLock = gl.getUniformLocation(program,"trany");
 
 	setUpTangram();
-	renderAllShapes();
+	render();
+	//renderAllShapes();
 };
 
 function render() {
@@ -85,9 +159,9 @@ function render() {
 	//tran += .1;
 	gl.uniform1f(tranxLock,tranx);
 	gl.uniform1f(tranyLock,trany);
-	
+	renderAllShapes();
     gl.drawArrays( gl.LINES, 0, size );
-	//window.requestAnimFrame(render);
+	window.requestAnimFrame(render);
 }
 
 function setUpTangram(){
@@ -97,28 +171,10 @@ function setUpTangram(){
 	triangleOne.push(vec2(-.5,-.5),vec2(0,0));
 	triangleOne.push(vec2(-.5,-.5),vec2(-.5,.5));
 	
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(triangleOne), gl.STATIC_DRAW );
-
-	var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-	
-	
 	triangleTwo.push(vec2(-.5,.5),vec2(.5,.5));
 	triangleTwo.push(vec2(.5,.5),vec2(0,0));
 	triangleTwo.push(vec2(0,0),vec2(-.5,.5));	
-	
-    var bufferIdT2 = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferIdT2 );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(triangleTwo), gl.STATIC_DRAW );
-	
-	var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-	//size = 6;
-	//render();
+
 	
 }
 
@@ -144,7 +200,7 @@ function renderAllShapes(){
 	for(var i = 0; i < triangleOne.length;i++){allShapes.push(triangleOne[i]);}
 	for(var i = 0; i < triangleTwo.length;i++){allShapes.push(triangleTwo[i]);}
 	
-	console.log(inside([0,.5],triangleTwo));
+	//console.log(inside([0,.5],triangleTwo));
 	size = allShapes.length;
 	
 	var finalbuffer = gl.createBuffer();
@@ -155,7 +211,7 @@ function renderAllShapes(){
 	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 	
-	render();
+	//render();
 }
 	
 	
